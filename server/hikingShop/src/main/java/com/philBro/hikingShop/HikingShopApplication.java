f3aadb7fe0c638f1ef5e280606884cf7c0a05d50
Phil Broadley <philbrocoding@hotmail.com>
2023-05-03 14:28:59 +0100

    Spring Boot backend initialised

---
+++ server/hikingShop/src/main/java/com/philBro/hikingShop/HikingShopApplication.java
@@ -0,0 +1,13 @@
+package com.philBro.hikingShop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HikingShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HikingShopApplication.class, args);
+	}
+
+}
---
+++ server/hikingShop/src/test/java/com/philBro/hikingShop/HikingShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.philBro.hikingShop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HikingShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
